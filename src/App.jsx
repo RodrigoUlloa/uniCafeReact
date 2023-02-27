bd72e1c1daf3a7243f9c1c399d99d61ed889e278
Rodrigo <rodrigo.ulloa22@gmail.com>
2023-02-26 21:31:16 -0300

    unicafe resolve, step 1,2,3

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import './App.css'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Statistics = (props) => (
+  <div>
+    <br></br>
+    <h2>Statistics</h2>
+    <p>good {props.good}</p>
+    <p>neutral {props.neutral}</p>
+    <p>bad {props.bad}</p>
+    <p>all {props.all}</p>
+    <p>average {props.average}</p>
+    <p>positive {(props.good*100)/(props.all)} %</p>
+  </div>
+)
+
+function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const all = good + neutral + bad
+  const average = (good + (-bad)) / (all)
+
+  return (
+    <div className="App">
+      <h2>Give feed back</h2>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      
+      <Statistics good={good} neutral ={neutral} bad = {bad} all = {all} average={average} / >
+    </div>
+  )
+}
+
+export default App
